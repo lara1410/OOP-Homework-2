207188fbceec09ce156a6c2055aabe3bc59385de
Larisa Abrahamyan <laraabrahamyan@yahoo.com>
2018-02-03 21:22:41 +0400

    Add files via upload

---
+++ pasta.java
@@ -0,0 +1,42 @@
+package OOP Homework 1;
+import java.util.*;
+class Main {
+
+  public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+		   System.out.println("Enter a number!");
+		int n = input.nextInt();
+		   while(n<1 || n>1000) {
+			    System.out.println("invalid number. Try again!");
+			       n = input.nextInt();
+		}
+		int nt=0;
+		int np=0;
+		int ns=0;
+		System.out.println("Enter pasta names " + n + " times!");
+		int i=1;
+		while(i<=n) {
+		    String str = input.next();
+		     str.toLowerCase().trim();
+		       if(str.equals("spagetti"))
+			 ns += 1;
+		      else if(str.equals("penne"))
+			 np += 1;
+			     else if(str.equals("tagliatelle"))
+			 nt += 1;
+
+		 if (str.equals("spagetti" || str.equals("penne") || str.equals("tagliatelle")))
+			 i++;
+		 else
+			System.out.println("Sorry, Type another.");
+		}
+		int max = Math.max(nt, Math.max(ns,np));
+		     if(max == np)
+			     System.out.println("Penne");
+		     else if(max == ns)
+			     System.out.println("Spagetti");
+		     else if(max == nt)
+			     System.out.println("Tagliatelle");
+		input.close();
+	}
+  }
---
+++ time.java
@@ -0,0 +1,35 @@
+package OOP Homework 1;
+import java.util.*;
+
+class Main {
+  	public static void main(String[] args) {
+		      Scanner input = new Scanner(System.in);
+		      System.out.println("hour");
+		      int jam = input.nextInt();;
+		      System.out.println("minutes");
+		      int rope = input.nextInt();;
+		      System.out.println("seconds");
+		      int vayr = input.nextInt();;
+		      if(jam > 23 || jam < 0 || rope > 59 || rope < 0 || vayr > 59 || vayr < 0)
+			    System.out.println("Invalid time!");
+				  else {
+					       if (jam != 23)
+					             jam += 1;
+					   else
+						              jam = 0;
+					                     if(rope == 59) {
+				                             rope = 0;
+                          			    if(vayr == 59) {
+			     	                             vayr = 0;
+				}
+				              else
+					                         rope += 1;
+					}
+				                  else
+			                             vayr +=1;
+
+			System.out.println(jam+":"+rope+":"+vayr);
+		}
+		input.close();
+	}
+			}
